8552bcc1139bb2f354702bfea8ebbba1e098cec7
Unknown <pisitpong.won@gmail.com>
2019-06-12 06:29:03 +0700

    Complete ReadUDP and Midball movement from received data

--- attention2players/Assets/MidballMovement.cs
+++ attention2players/Assets/MidballMovement.cs
@@ -7,42 +7,93 @@ public class MidballMovement : MonoBehaviour
 	public Rigidbody rb;
 	public Rigidbody playerLeft;
 	public Vector3 a, b, ltran, mtran;
-	public float centerDist; // will be change to position from UDP
+	private ReadUDP readUDP;
+	public float centerDist, centerVelo, centerDiff, data1, data2, factor1, factor2;
 	public float v, j, tumble;
 	public int con = 0;
 	void Start()
 	{
 		print("Midball start");
+		GameObject checkGameObject = GameObject.FindGameObjectWithTag ("UDPReceiver");
+        if (checkGameObject != null)
+        {
+            readUDP = checkGameObject.GetComponent <ReadUDP>();
+        }
+        if (readUDP == null)
+        {
+            Debug.Log("Cannot find 'ReadUDP' script");
+        }
 	}
 	void Update () 
 	{
 		if(GameObject.Find("Player Left")!=null && GameObject.Find("Player Right")!=null)
 		{
-			ltran = playerLeft.transform.position;
-			mtran = rb.transform.position;
+			//Movement1();
 
-			// Midball controller
-			//mtran.x = ltran.x + centerDist;
-			a = rb.velocity;
-			b = rb.angularVelocity;
-			a.z = 0f;
+			//Movement2();
+
+			MidballMove();
+		}
+	}
+	void Movement1()
+	{
+		ltran = playerLeft.transform.position;
+		mtran = rb.transform.position;
+
+		data1 = readUDP.data1float;	// Ref data1 from UDP
+		data2 = readUDP.data2float; // Ref data2 from UDP
+		centerDiff = data1 - data2; // Different power
+
+		// Position setting
+		centerDist = (centerDiff)*factor1;  // *** Power scaling with using range ***
+
+		mtran.x = ltran.x + centerDist; // Set distance from Left Player
+		rb.transform.position = mtran;
+
+		// Rotation setting
+		b = rb.angularVelocity;
+		if(centerDiff > 0) {b.z = -tumble; rb.angularVelocity = b;}
+		else if(centerDiff < 0) {b.z = tumble; rb.angularVelocity = b;}
+		else {b.z = 0; rb.angularVelocity = b;}
+	}
+	void Movement2()
+	{
+		data1 = readUDP.data1float;	// Ref data1 from UDP
+		data2 = readUDP.data2float; // Ref data2 from UDP
+		centerDiff = data1 - data2; // Different power
+
+		// Velocity setting
+		a = rb.velocity;
+		a.z = 0f;
+		float centerVelo = (centerDiff)*factor2; // *** Power scaling with using range ***
+		a.x = centerVelo; // Set velocity of Midball
+		rb.velocity = a;
+
+		// Rotation setting
+		b = rb.angularVelocity;
+		if(centerDiff > 0) {b.z = -tumble; rb.angularVelocity = b;}
+		else if(centerDiff < 0) {b.z = tumble; rb.angularVelocity = b;}
+		else {b.z = 0; rb.angularVelocity = b;}
+	}
+	void MidballMove()
+	{
+		a = rb.velocity;
+		b = rb.angularVelocity;
+		a.z = 0f;
+		rb.velocity = a;
+		if(Input.GetKey("right"))
+		{
+			a.x = v;
+			b.z = -tumble;
 			rb.velocity = a;
-			if(Input.GetKey("right"))
-			{
-				a.x = v;
-				b.z = -tumble;
-				rb.velocity = a;
-				rb.angularVelocity = b;
-			}
-			if(Input.GetKey("left"))
-			{
-				a.x = -v;
-				b.z = tumble;
-				rb.velocity = a;
-				rb.angularVelocity = b;
-			}
-
-			rb.transform.position = mtran;
+			rb.angularVelocity = b;
+		}
+		if(Input.GetKey("left"))
+		{
+			a.x = -v;
+			b.z = tumble;
+			rb.velocity = a;
+			rb.angularVelocity = b;
 		}
 	}
 }
---
+++ attention2players/Assets/ReadUDP.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using UnityEngine;
+
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+public class ReadUDP : MonoBehaviour
+ {
+
+	 // port
+	 public int portLocal1 = 8000;
+	 public int portLocal2 = 8001;
+
+	// UDP client objects
+	 UdpClient client1;
+	 UdpClient client2;
+
+	// Receiving thread
+	Thread receiveThread;
+	private bool onReceive = true;
+
+	private float timeshift = 0;
+	public string data1hex;
+	public string data2hex;
+	public float data1float;
+	public float data2float;
+	Encoding utf8 = Encoding.UTF8;
+	void Start () 
+	{
+		init();
+	}
+
+	private void init()
+	{
+		print("UDP object init");
+
+		// Create local client
+		client1 = new UdpClient(portLocal1);
+		client2 = new UdpClient(portLocal2);
+
+		// Create new thread for reception of incoming data
+		receiveThread = new Thread (
+			new ThreadStart (ReceiveData));
+		receiveThread.IsBackground = true;
+		receiveThread.Start();
+		print("ReadUDP is listening");
+	}
+
+	// Receive data, update received packages
+	private void ReceiveData()
+	{
+		print("ReceiveData func");
+		do
+		{
+			try
+			{
+				IPEndPoint IP8000 = new IPEndPoint(IPAddress.Any, portLocal1);
+				IPEndPoint IP8001 = new IPEndPoint(IPAddress.Any, portLocal2);
+				print("Pass IP");
+
+				byte[] data1 = client1.Receive(ref IP8000);
+				byte[] data2 = client2.Receive(ref IP8001);
+				print("Pass receiving data");
+
+				data1hex = BitConverter.ToString(data1, 0); // hex string
+				data2hex = BitConverter.ToString(data2, 0); // hex string
+				string data1utf8 = utf8.GetString(data1);	// dec string
+				string data2utf8 = utf8.GetString(data2);	// dec string
+				data1float = float.Parse(data1utf8); 		// float
+				data2float = float.Parse(data2utf8);		// float
+				print("Data1hex : " + data1hex);
+				print("Data1dec : " + data1utf8);
+				print("Data1f : " + data1float);
+				print("Data2hex : " + data2hex);
+				print("Data2dec : " + data2utf8);
+				print("Data2f : " + data2float);
+
+				print("Pass bit converter");
+			}
+			catch(Exception err)
+			{
+				print(err.ToString());
+			}
+		}while(onReceive);
+	}
+	void OnDisable()
+	{
+		onReceive = false;
+		if(receiveThread != null)
+			receiveThread.Abort();
+		client1.Close();
+		client2.Close();
+		print("Close all");
+	}
+}
